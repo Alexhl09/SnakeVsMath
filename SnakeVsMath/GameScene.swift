527c4699e73d6d1c70e8c4bf254a073e2de6c239
Alejandro Hernandez <alexhl1999@hotmail.com>
2018-04-05 23:12:03 -0500

    Initial Commit

---
+++ SnakeVsMath/GameScene.swift
@@ -0,0 +1,418 @@
+//
+//  GameScene.swift
+//  SnakeVsMath
+//
+//  Created by Alejandro on 05/04/18.
+//  Copyright © 2018 AleyBrenda. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+struct CuerpoFisico {
+    static let cabeza : UInt32 = 0x1 << 0
+    static let operacion : UInt32 = 0x1 << 1
+    static let cola : UInt32 = 0x1 << 2
+    static let respuesta : UInt32 = 0x1 << 3
+    static let respuestaCorrecta : UInt32 = 0x1 << 4
+}
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    private var cabeza = SKSpriteNode()
+    private var operacionLabel = SKLabelNode()
+    private var resupuesta = SKSpriteNode()
+    private var textRespuesta = SKLabelNode()
+    private var textMalaRes = SKLabelNode()
+    private var cola1 = SKSpriteNode()
+    private var cola = [SKSpriteNode()]
+    private var numero = [Bool](repeating: false, count: 20)
+    private var index = 0
+    private var background = SKSpriteNode(imageNamed: "Orange_BG")
+     var numeroColas = 0
+     var numeroVidas = 0
+    private var gana = SKSpriteNode()
+    private var pierde = SKSpriteNode()
+    private var respuestaCorrecta = SKSpriteNode()
+    private var res = ""
+    var numero1 = Int()
+    var numero2 = Int()
+    
+    func crearCabeza()
+    {
+    
+        cabeza = SKSpriteNode(imageNamed: "cabeza_snake")
+        cabeza.position = CGPoint(x: 0, y: -150)
+        cabeza.setScale(0.5)
+        cabeza.physicsBody = SKPhysicsBody(rectangleOf: cabeza.size)
+        cabeza.physicsBody?.categoryBitMask = CuerpoFisico.cabeza
+        cabeza.physicsBody?.collisionBitMask = CuerpoFisico.respuesta | CuerpoFisico.respuestaCorrecta
+       cabeza.physicsBody?.contactTestBitMask = CuerpoFisico.respuesta | CuerpoFisico.respuestaCorrecta
+        cabeza.physicsBody?.isDynamic = true
+        cabeza.physicsBody?.affectedByGravity = false
+        self.addChild(cabeza)
+    }
+    func crearOperacion()
+    {
+        numero1 = GKRandomDistribution(lowestValue: 0, highestValue: 99).nextInt()
+        numero2 = GKRandomDistribution(lowestValue: 0, highestValue: 99).nextInt()
+        
+        var string = "\(numero1) + \(numero2)"
+   
+        operacionLabel = SKLabelNode(text: string)
+        operacionLabel.fontSize =  45
+        operacionLabel.position = CGPoint(x: 0, y: 400)
+        operacionLabel.fontName = "Arial-BoldMT"
+        operacionLabel.physicsBody?.isDynamic = false
+        operacionLabel.physicsBody?.affectedByGravity = false
+        self.addChild(operacionLabel)
+    }
+    func crearCola()
+    {
+        
+        cola1 = SKSpriteNode(imageNamed: "cuerpo_snake")
+        
+        switch numeroColas {
+        case 0:
+            cola1.position = CGPoint(x: cabeza.position.x, y:-220 )
+            break
+        case 1:
+            cola1.position = CGPoint(x: 0, y:-250 )
+            break
+        case 2:
+            cola1.position = CGPoint(x: 0, y:-280 )
+            break
+        case 3:
+            cola1.position = CGPoint(x: 0, y:-310 )
+            break
+        case 4:
+            cola1.position = CGPoint(x: 0, y:-340 )
+            break
+        case 5:
+            cola1.position = CGPoint(x: 0, y:-370 )
+        case 6:
+            cola1.position = CGPoint(x: 0, y:-400 )
+            break
+        case 7:
+            cola1.position = CGPoint(x: 0, y:-430 )
+            break
+        case 8:
+            cola1.position = CGPoint(x: 0, y:-460 )
+            break
+        case 9:
+            cola1.position = CGPoint(x: 0, y:-590 )
+            break
+        case 10:
+            cola1.position = CGPoint(x: 0, y:-600 )
+            break
+        case 11:
+            cola1.position = CGPoint(x: 0, y:-650 )
+            break
+        case 12:
+            cola1.position = CGPoint(x: 0, y:-350 )
+            break
+        case 13:
+            cola1.position = CGPoint(x: 0, y:-110 )
+            break
+        case 14:
+            cola1.position = CGPoint(x: 0, y:140 )
+            break
+        case 15:
+            cola1.position = CGPoint(x: 0, y:270 )
+            break
+        default:
+            break
+        }
+        
+        cola1.setScale(0.5)
+        cola1.physicsBody = SKPhysicsBody(rectangleOf: cola1.size)
+        cola1.physicsBody?.affectedByGravity = false
+        cola1.physicsBody?.isDynamic = false
+        
+        self.cola.append(cola1)
+        self.addChild(self.cola[numeroColas])
+        numero[index] = true
+        index = index + 1
+        numeroColas = numeroColas + 1
+        
+    }
+    func crearMensajeGanador()
+    {
+        gana = SKSpriteNode(imageNamed: "ganaste")
+        gana.setScale(0.6)
+        gana.position = CGPoint(x: 0, y: 0)
+        gana.zPosition = -1
+        addChild(gana)
+    }
+    func crearMensajePerdedor()
+    {
+        pierde = SKSpriteNode(imageNamed: "perdiste")
+        pierde.setScale(0.6)
+        pierde.position = CGPoint(x: 0, y: 0)
+        pierde.zPosition = -1
+        addChild(pierde)
+    }
+    
+    override func sceneDidLoad() {
+        self.physicsWorld.contactDelegate = self
+        self.physicsWorld.gravity = CGVector(dx: 0, dy: -0.7)
+        background.position = CGPoint(x: frame.size.width / 2, y: frame.size.height / 2)
+        background.zPosition = -1
+        addChild(background)
+        crearCabeza()
+        crearOperacion()
+        crearCola()
+        
+        let pregunta = SKAction.run({
+            ()
+            in
+            self.operacionLabel.removeFromParent()
+            self.crearOperacion()
+        })
+        let del = SKAction.wait(forDuration: 7)
+        let spawn = SKAction.sequence([pregunta, del])
+        let spawnForever = SKAction.repeatForever(spawn)
+        self.run(spawnForever, withKey: "pregunta")
+        
+        let respuestaAction = SKAction.run({
+            ()
+            in self.crearResultNode()
+        })
+        let delay = SKAction.wait(forDuration: 7)
+        let spawnDelay = SKAction.sequence([respuestaAction, delay])
+        let spawnDelayForever = SKAction.repeatForever(spawnDelay)
+        self.run(spawnDelayForever, withKey: "respuesta")
+        
+        let respuestaCorrectaAction = SKAction.run({
+            ()
+            in self.crearResultCorrectaNode()
+        })
+        let delatC = SKAction.wait(forDuration: 7)
+        let spawnDelayC = SKAction.sequence([respuestaCorrectaAction, delatC])
+        let spawnDelayForeverC = SKAction.repeatForever(spawnDelayC)
+        self.run(spawnDelayForeverC, withKey: "respuestaCorrecta")
+        
+      
+    }
+    func crearResultNode()
+    {
+
+        resupuesta = SKSpriteNode(imageNamed: "punto")
+       
+        resupuesta.setScale(0.5)
+        resupuesta.physicsBody = SKPhysicsBody(rectangleOf: resupuesta.size)
+        resupuesta.physicsBody?.categoryBitMask = CuerpoFisico.respuesta
+        resupuesta.physicsBody?.collisionBitMask = CuerpoFisico.cabeza
+        resupuesta.physicsBody?.contactTestBitMask = CuerpoFisico.cabeza
+        resupuesta.physicsBody?.isDynamic = true
+        let miRandomPosition = GKRandomDistribution(lowestValue: -450, highestValue: 400)
+        let posicionXRespuesta  = CGFloat(miRandomPosition.nextInt())
+        resupuesta.physicsBody?.affectedByGravity = false
+        resupuesta.position = CGPoint(x:posicionXRespuesta, y: 500)
+        self.addChild(resupuesta)
+      var sum = Int(arc4random_uniform(30))
+        
+        res = "\(numero1 + numero2 + sum)"
+        print(res)
+        textMalaRes = SKLabelNode(text: res)
+        textMalaRes.fontSize =  45
+        textMalaRes.position = CGPoint(x: resupuesta.position.x, y: resupuesta.position.y)
+        textMalaRes.fontName = "Arial-BoldMT"
+        textMalaRes.zPosition = 4
+        
+        self.addChild(textMalaRes)
+       
+        let animacionTime = 5
+        var arrayDeAcciones = [SKAction]()
+        
+        arrayDeAcciones.append(SKAction.move(to: CGPoint(x:posicionXRespuesta, y: -(self.frame.size.height + 30)), duration: TimeInterval(animacionTime)))
+        
+        arrayDeAcciones.append(SKAction.removeFromParent())
+        
+        resupuesta.run(SKAction.sequence(arrayDeAcciones))
+        textMalaRes.run(SKAction.sequence(arrayDeAcciones))
+    }
+    
+    func crearResultCorrectaNode()
+    {
+        
+        
+        respuestaCorrecta = SKSpriteNode(imageNamed: "punto")
+        respuestaCorrecta.shader = SKShader(fileNamed: "Green_BG")
+        respuestaCorrecta.setScale(0.5)
+        respuestaCorrecta.physicsBody = SKPhysicsBody(rectangleOf: respuestaCorrecta.size)
+        respuestaCorrecta.physicsBody?.categoryBitMask = CuerpoFisico.respuestaCorrecta
+        respuestaCorrecta.physicsBody?.collisionBitMask = CuerpoFisico.cabeza
+        respuestaCorrecta.physicsBody?.contactTestBitMask = CuerpoFisico.cabeza
+        respuestaCorrecta.physicsBody?.isDynamic = true
+        let miRandomPosition = GKRandomDistribution(lowestValue: -450, highestValue: 400)
+        let posicionXRespuesta  = CGFloat(miRandomPosition.nextInt())
+        respuestaCorrecta.physicsBody?.affectedByGravity = false
+        respuestaCorrecta.position = CGPoint(x:posicionXRespuesta, y: 500)
+       
+        self.addChild(respuestaCorrecta)
+        res = "\(numero1 + numero2)"
+             print(res)
+        textRespuesta = SKLabelNode(text: res)
+        textRespuesta.fontSize =  45
+        textRespuesta.position = CGPoint(x: respuestaCorrecta.position.x, y: respuestaCorrecta.position.y)
+        textRespuesta.zPosition = 4
+        textRespuesta.fontName = "Arial-BoldMT"
+        
+        self.addChild(textRespuesta)
+        
+        let animacionTime = 5
+        var arrayDeAcciones = [SKAction]()
+        
+        arrayDeAcciones.append(SKAction.move(to: CGPoint(x:posicionXRespuesta, y: -(self.frame.size.height + 30)), duration: TimeInterval(animacionTime)))
+        
+        arrayDeAcciones.append(SKAction.removeFromParent())
+        textRespuesta.run(SKAction.sequence(arrayDeAcciones))
+        respuestaCorrecta.run(SKAction.sequence(arrayDeAcciones))
+    }
+    
+    
+    
+   
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches{
+            let location = touch.location(in: self)
+            
+            cabeza.run(SKAction.moveTo(x: location.x, duration: 0.2))
+            for i in cola
+            {
+                 i.run(SKAction.moveTo(x: cabeza.position.x, duration: 0.4))
+            }
+           
+        }
+
+        
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches{
+            let location = touch.location(in: self)
+            
+            cabeza.run(SKAction.moveTo(x: location.x, duration: 0.2))
+            for i in cola
+            {
+             i.run(SKAction.moveTo(x: cabeza.position.x, duration: 0.2))
+            }
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+      
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+       
+    }
+ 
+    override func update(_ currentTime: TimeInterval) {
+        
+    }
+    func didBegin(_ contact: SKPhysicsContact) {
+        let firstBody = contact.bodyA
+        let secondBody = contact.bodyB
+       
+       
+        if firstBody.categoryBitMask == CuerpoFisico.cabeza && secondBody.categoryBitMask == CuerpoFisico.respuesta ||
+            firstBody.categoryBitMask == CuerpoFisico.respuesta && secondBody.categoryBitMask == CuerpoFisico.cabeza
+        {
+            if numeroColas - 1 > 0
+            {
+            if firstBody.categoryBitMask == CuerpoFisico.respuesta
+            {
+                
+                resupuesta.removeFromParent()
+                cola[numeroColas].removeFromParent()
+                numeroColas = numeroColas - 1
+                numeroVidas =  numeroVidas - 1
+                resupuesta.removeFromParent()
+            }
+            else if secondBody.categoryBitMask == CuerpoFisico.respuesta
+            {
+                resupuesta.removeFromParent()
+                cola[numeroColas].removeFromParent()
+                numeroColas = numeroColas - 1
+                numeroVidas = numeroVidas - 1
+                resupuesta.removeFromParent()
+            }
+            }
+            else
+            {
+                print("acabado")
+                removeAllActions()
+                removeAllChildren()
+                resupuesta.removeFromParent()
+                removeAction(forKey: "respuesta")
+                removeAction(forKey: "pregunta")
+                removeAction(forKey: "respuestaCorrecta")
+                crearMensajePerdedor()
+            }
+        }
+            
+        
+        
+       else if firstBody.categoryBitMask == CuerpoFisico.cabeza && secondBody.categoryBitMask == CuerpoFisico.respuestaCorrecta ||
+            firstBody.categoryBitMask == CuerpoFisico.respuestaCorrecta && secondBody.categoryBitMask == CuerpoFisico.cabeza
+        {
+            print("RC")
+            
+            print(index)
+            print(numeroColas)
+            print(numeroVidas)
+            
+            if numeroVidas < 10
+            {
+            if firstBody.categoryBitMask == CuerpoFisico.respuestaCorrecta
+            {
+                respuestaCorrecta.removeFromParent()
+                numeroVidas =  numeroVidas + 1
+                textRespuesta.removeFromParent()
+                textMalaRes.removeFromParent()
+                if numero[numeroColas + 1] == true
+                {
+                    print(true)
+                    numeroColas = numeroColas + 1
+                  self.addChild(cola[numeroColas])
+                }
+                else
+                {
+                crearCola()
+                }
+                
+                
+            }
+            else if secondBody.categoryBitMask == CuerpoFisico.respuestaCorrecta
+            {
+                textRespuesta.removeFromParent()
+                respuestaCorrecta.removeFromParent()
+                textMalaRes.removeFromParent()
+                numeroVidas =  numeroVidas + 1
+                if numero[numeroColas + 1] == true
+                {
+                    print(true)
+                    numeroColas = numeroColas + 1 
+                    self.addChild(cola[numeroColas])
+                }
+                else
+                {
+                    crearCola()
+                }
+                
+                
+          
+            }
+            }
+            else
+            {
+                crearMensajeGanador()
+            }
+            
+        }
+            
+        }
+        
+    }
+
---
+++ SnakeVsMath/GameViewController.swift
@@ -0,0 +1,71 @@
+//
+//  GameViewController.swift
+//  SnakeVsMath
+//
+//  Created by Alejandro on 05/04/18.
+//  Copyright © 2018 AleyBrenda. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+    var stopGame = GameScene()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
+        // including entities and graphs.
+        if let scene = GKScene(fileNamed: "GameScene") {
+            
+            // Get the SKScene from the loaded GKScene
+            if let sceneNode = scene.rootNode as! GameScene? {
+                
+               
+                
+                // Set the scale mode to scale to fit the window
+                sceneNode.scaleMode = .aspectFill
+                
+                // Present the scene
+                if let view = self.view as! SKView? {
+                    view.presentScene(sceneNode)
+                    
+                    view.ignoresSiblingOrder = true
+                    
+                    view.showsFPS = true
+                    view.showsNodeCount = true
+                }
+            }
+        }
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    @IBAction func salir(_ sender: UIButton) {
+        stopGame.removeAllActions()
+        stopGame.removeAllChildren()
+        stopGame.numeroColas = 0
+        stopGame.numeroVidas = 0
+    }
+    
+}
